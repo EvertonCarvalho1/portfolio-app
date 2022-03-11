4c212440d7734e401a427520e1f0da45ed6d898c
Everton Carvalho <everton.carvalho.dc@gmail.com>
2022-03-11 13:41:11 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
+
+
+export default createGlobalStyle`
+
+*{
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+}
+
+body{
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, button{
+    font: 16px 'Roboto', sans-serif;
+}
+button{
+    cursor: pointer;
+}
+
+`;
