beeffd9372d9298746b7f9cce1a0ddf4d50b17fc
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-04-01 19:32:21 -0300

    my skills container

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header";
 import AboutMe from "../AboutMe";
 import Home from "../Home";
+import MySkils from "../MySkils";
 import { Container } from "./styles";
 
 const Main: React.FC = () => {
@@ -11,6 +12,7 @@ const Main: React.FC = () => {
                 <Header />
                 <Home />
                 <AboutMe />
+                <MySkils />
             </Container>
         </>
     )
---
+++ src/pages/MySkils/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "./styles";
+
+const MySkils: React.FC = () => {
+    return (
+        <Container>
+            <h1>MySkils</h1>
+        </Container>
+    )
+}
+
+export default MySkils;
+
+
